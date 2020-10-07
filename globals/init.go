7ea0afb2754c2d969e8add871a7647ae203d9ab3
LIU JIAN <onanying@126.com>
2020-10-07 15:37:03 +0800

    feat: update init

--- globals/init.go
+++ globals/init.go
@@ -4,14 +4,23 @@ import (
     "fmt"
     "github.com/mix-go/console"
     "github.com/mix-go/logrus"
+    logrus2 "github.com/sirupsen/logrus"
     "io"
     "os"
 )
 
 func Init() {
-    // 日志配置
+    // logger
     logger := Logger()
     file := logrus.NewFileWriter(fmt.Sprintf("%s/../runtime/logs/test.log", console.App().BasePath), 7)
     writer := io.MultiWriter(os.Stdout, file)
     logger.SetOutput(writer)
+    if console.App().AppDebug {
+        logger.SetLevel(logrus2.DebugLevel)
+    }
+
+    // db
+    db := DB()
+    db.SetLogger(logger)
+    db.LogMode(console.App().AppDebug)
 }
